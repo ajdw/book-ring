a0c0a60b89e5b5c5f9835d3b69d591389ad01ccd
Alex Williams <ajdw1987@gmail.com>
2019-05-14 16:47:14 -0700

    added notes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+
+// create a variable called circle and set it equal to the circle element we find in our html
 var circle = document.querySelector('circle');
+/* create a variable called radius and set it equal to the defined circle variable above with the radius (r) 
+pointing at the baseVal */
 var radius = circle.r.baseVal.value;
 var circumference = radius * 2 * Math.PI;
 
@@ -10,7 +14,9 @@ function setProgress(percent) {
   circle.style.strokeDashoffset = offset;
 }
 
+/* we create a const named input equal to the document's input */
 const input = document.querySelector('input');
+// we pass in our function the value we've gathered from the input value
 setProgress(input.value);
 
 input.addEventListener('change', function(e) {
